076c948d6255d984806acfb05e1628c5862414cf
Eddi1405 <77676438+Eddi1405@users.noreply.github.com>
2023-04-27 11:42:28 +0200

    start

---
+++ src/main/java/thowl/wiprojekt/WiProjektApplication.java
@@ -0,0 +1,13 @@
+package thowl.wiprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thowl/wiprojekt/WiProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package thowl.wiprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
