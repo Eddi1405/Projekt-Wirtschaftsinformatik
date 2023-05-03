901cf5f444fef5298131ec44cc8b5d754dcd6a9f
Eddi1405 <77676438+Eddi1405@users.noreply.github.com>
2023-05-03 18:43:35 +0200

    Service fixes

--- Backend/src/main/java/thowl/wiprojekt/service/userService.java
+++ Backend/src/main/java/thowl/wiprojekt/service/userService.java
@@ -3,15 +3,17 @@ package thowl.wiprojekt.service;
 import thowl.wiprojekt.entity.userData;
 import thowl.wiprojekt.repository.userJpaRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class userService {
 
-    @Autowired
-    private userJpaRepository uJR;
+    private final userJpaRepository uJR;
+
+    public userService(userJpaRepository uJR) {
+        this.uJR = uJR;
+    }
 
     public void saveRegisterData(String username, String password, String email, String roles) {
         userData uData = new userData();
