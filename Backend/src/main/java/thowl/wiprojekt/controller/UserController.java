89c070a7dcb4a3b1a00a342193cd1b74422343fa
Flo7880 <florian-laufer@web.de>
2023-05-10 18:54:46 +0200

    UserController getUser() deleteUser()

--- Backend/src/main/java/thowl/wiprojekt/controller/UserController.java
+++ Backend/src/main/java/thowl/wiprojekt/controller/UserController.java
@@ -1,20 +1,25 @@
 package thowl.wiprojekt.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import thowl.wiprojekt.entity.User;
+import thowl.wiprojekt.entity.model.UserModelAssembler;
 import thowl.wiprojekt.errors.ResourceNotFoundException;
 import thowl.wiprojekt.repository.UserRepository;
 import thowl.wiprojekt.service.UserService;
 
+@Slf4j
 @RestController
 public class UserController {
 
     private final UserRepository UR;
     private final UserService US;
+
+    @Autowired
+    private UserModelAssembler assembler;
 
     public UserController(UserRepository ur, UserService us) {
         UR = ur;
@@ -41,5 +46,24 @@ public class UserController {
         //user.add(selfLink);
         return ResponseEntity.ok(user);
     }
+
+    /**
+     * Deletes a user with the given ID.
+     *
+     * @param id the ID of the user to delete
+     * @return a ResponseEntity with an HTTP status code of 204 (No Content) if the user was deleted successfully
+     * @throws ResourceNotFoundException if the user does not exist
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        return UR.findById(id)
+                .map(user -> {
+                    UR.delete(user);
+                    return ResponseEntity.noContent().build();
+                })
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id " + id));
+    }
+
+
 
 }
