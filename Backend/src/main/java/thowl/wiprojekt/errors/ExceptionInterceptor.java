cd7fb74650bbfc6cb211f1a1247515087a7a8448
G00d0lJava <102469957+G00d0lJava@users.noreply.github.com>
2023-05-14 17:01:07 +0200

    Added logging functionality
    Added central log directory and logging of Exceptions

--- Backend/src/main/java/thowl/wiprojekt/errors/ExceptionInterceptor.java
+++ Backend/src/main/java/thowl/wiprojekt/errors/ExceptionInterceptor.java
@@ -1,10 +1,14 @@
 package thowl.wiprojekt.errors;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 /**
  * <quote>Intercepts</quote> thrown {@link Exception}s to return
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * @author Michael Hartmann
  * @version 06.05.2023
  */
+@Slf4j
 @ControllerAdvice
 public class ExceptionInterceptor {
 
@@ -27,6 +32,7 @@ public class ExceptionInterceptor {
 	@ResponseStatus(HttpStatus.CONFLICT)
 	@ExceptionHandler(ResourceAlreadyExistsException.class)
 	public String interceptExistingResource(ResourceAlreadyExistsException e) {
+		this.logException(e, false);
 		return e.getMessage();
 	}
 
@@ -41,6 +47,7 @@ public class ExceptionInterceptor {
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public String interceptNotFound(ResourceNotFoundException e) {
+		this.logException(e, false);
 		return e.getMessage();
 	}
 
@@ -55,6 +62,7 @@ public class ExceptionInterceptor {
 	@ResponseStatus(HttpStatus.UNAUTHORIZED)
 	@ExceptionHandler(RestAuthenticationException.class)
 	public String interceptRights(RestAuthenticationException e) {
+		this.logException(e, false);
 		return e.getMessage();
 	}
 
@@ -70,6 +78,7 @@ public class ExceptionInterceptor {
 	@ExceptionHandler(InsufficientRightsException.class)
 	public String interceptRights(InsufficientRightsException e) {
 //		var p = new IllegalEntityException(new UserData());
+		this.logException(e, false);
 		return e.getMessage();
 	}
 
@@ -84,6 +93,7 @@ public class ExceptionInterceptor {
 	@ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
 	@ExceptionHandler(MethodNotSupportedException.class)
 	public String interceptRights(MethodNotSupportedException e) {
+		this.logException(e, false);
 		return e.getMessage();
 	}
 
@@ -98,6 +108,7 @@ public class ExceptionInterceptor {
 	@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
 	@ExceptionHandler(IllegalEntityException.class)
 	public String interceptIllegalEntity(IllegalEntityException e) {
+		this.logException(e, false);
 		return e.getMessage();
 	}
 
@@ -112,6 +123,7 @@ public class ExceptionInterceptor {
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler(InternalException.class)
 	public String interceptInternal(InternalException e) {
+		this.logException(e, true);
 		return e.getMessage();
 	}
 
@@ -126,6 +138,29 @@ public class ExceptionInterceptor {
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(MalformedRequestException.class)
 	public String interceptMalformedRequest(MalformedRequestException e) {
+		this.logException(e, false);
 		return e.getMessage();
 	}
+
+	/**
+	 * Logs the stacktrace of an {@link Exception}.
+	 *
+	 * @param e The {@link Exception} to be logged.
+	 * @param error <code>true</code> if the {@link Exception} should be logged
+	 * as an error, <code>false</code> otherwise.
+	 */
+	private void logException(Exception e, boolean error) {
+		// Used to convert stacktrace to String
+		// idea from https://www.baeldung.com/java-stacktrace-to-string
+		StringWriter stringer = new StringWriter();
+		PrintWriter writer = new PrintWriter(stringer);
+		e.printStackTrace(writer);
+		if (error) {
+			log.error(stringer.toString());
+		}
+		else {
+			log.warn(stringer.toString());
+		}
+	}
+
 }
