ef452ce36816c543855cd7494020136f713a4301
Flo7880 <99340436+Flo7880@users.noreply.github.com>
2023-05-16 19:26:37 +0200

    CommentController (untested)

--- Backend/src/main/java/thowl/wiprojekt/controller/CommentController.java
+++ Backend/src/main/java/thowl/wiprojekt/controller/CommentController.java
@@ -1,13 +1,12 @@
 package thowl.wiprojekt.controller;
 
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import thowl.wiprojekt.entity.Comment;
-import thowl.wiprojekt.repository.CommentRepository;
+
 import thowl.wiprojekt.service.CommentService;
 
 import java.util.List;
