d81187306867b24b3bbe4bf16592659271d2fc1f
G00d0lJava <102469957+G00d0lJava@users.noreply.github.com>
2023-05-20 13:38:58 +0200

    Added ModelAssembler Stubs

---
+++ Backend/src/main/java/thowl/wiprojekt/entity/model/CommentModelAssembler.java
@@ -0,0 +1,31 @@
+package thowl.wiprojekt.entity.model;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+import thowl.wiprojekt.entity.Comment;
+
+/**
+ * Creates {@link EntityModel}s from {@link Comment} objects.
+ *
+ * @version 20.05.2023
+ */
+@Component
+public class CommentModelAssembler implements
+		RepresentationModelAssembler<Comment, EntityModel<Comment>> {
+
+	// TODO stub
+
+	/**
+	 * Converts the specified {@link Comment} to an {@link EntityModel}.
+	 *
+	 * @param entity The {@link Comment} to be converted.
+	 * @return The {@link EntityModel} representation of the specified
+	 * {@link Comment}.
+	 */
+	@Override
+	public EntityModel<Comment> toModel(Comment entity) {
+		EntityModel<Comment> model = EntityModel.of(entity);
+		return model;
+	}
+}
