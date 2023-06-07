e4115adb2d58ccec34b9e7fd6c22b5a5acfffa22
G00d0lJava <102469957+G00d0lJava@users.noreply.github.com>
2023-06-07 16:15:36 +0200

    Maybe fixed websocket not working

--- Backend/src/main/java/thowl/wiprojekt/config/ChatConfig.java
+++ Backend/src/main/java/thowl/wiprojekt/config/ChatConfig.java
@@ -2,6 +2,7 @@ package thowl.wiprojekt.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -13,6 +14,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
  */
 @Configuration
 @EnableWebSocketMessageBroker
+@Controller
 public class ChatConfig implements WebSocketMessageBrokerConfigurer {
 
 	/*
