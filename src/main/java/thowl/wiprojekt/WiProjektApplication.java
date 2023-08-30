64ff2229031cf04975a256685fc7e2b40fe3630b
Eddi1405 <77676438+Eddi1405@users.noreply.github.com>
2023-08-30 19:07:05 +0200

    Frontend Fix

--- src/main/java/thowl/wiprojekt/WiProjektApplication.java
+++ src/main/java/thowl/wiprojekt/WiProjektApplication.java
@@ -2,12 +2,28 @@ package thowl.wiprojekt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WiProjektApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WiProjektApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*");
+			}
+		};
 	}
 
 }
