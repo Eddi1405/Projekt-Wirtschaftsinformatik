53fb15b04564531c6f00e590573ec21173187e04
Eddi1405 <77676438+Eddi1405@users.noreply.github.com>
2023-09-06 15:21:41 +0200

    Update WiProjektApplication.java

--- src/main/java/thowl/wiprojekt/WiProjektApplication.java
+++ src/main/java/thowl/wiprojekt/WiProjektApplication.java
@@ -18,7 +18,7 @@ public class WiProjektApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
+				registry.addMapping("/*")
 						.allowedOrigins("*")
 						.allowedMethods("*")
 						.allowedHeaders("*");
