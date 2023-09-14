dbd9d44b96e4522ef502940a7f89a2d507a9e1c5
G00d0lJava <102469957+G00d0lJava@users.noreply.github.com>
2023-09-14 12:52:09 +0200

    Fixed setup scripts

--- src/main/resources/setup/websocket_chatup.cmd
+++ src/main/resources/setup/websocket_chatup.cmd
@@ -10,7 +10,7 @@ rem Add room chat
 curl -v -X POST localhost:8080/chats/create -H "Content-type:application/json" -d "{\"chatName\":\"Room Chat\",\"chatType\":\"room\"}"
 rem Register the two users
 curl -v -X PATCH localhost:8080/chats/register/1 -H "Content-type:application/json" -d 1
-curl -v -X PATCH localhost:8080/chats/register/1 -H "Content-type:application/json" -d 1
+curl -v -X PATCH localhost:8080/chats/register/1 -H "Content-type:application/json" -d 2
 rem Add new Chat none of the users are registered with
 curl -v -X POST localhost:8080/chats/create -H "Content-type:application/json" -d "{\"chatName\":\"OtherChat\",\"chatType\":\"personal\"}"
 pause
